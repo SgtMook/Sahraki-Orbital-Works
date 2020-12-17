7c0feb7f4b7527773054e24d60fc40c4f1b504f7
SgtMook <SgtMook314@gmail.com>
2020-12-16 21:53:42 -0800

    Bunch of more work on tactical map subsystem

--- SharedProjects/Subsystems/TacMapSubsystem.cs
+++ SharedProjects/Subsystems/TacMapSubsystem.cs
@@ -26,6 +26,7 @@ namespace IngameScript
 
         public void Command(TimeSpan timestamp, string command, object argument)
         {
+            if (command == "togglenorth") AlignWithNorth = !AlignWithNorth;
         }
 
         public void DeserializeSubsystem(string serialized)
@@ -34,7 +35,7 @@ namespace IngameScript
 
         public string GetStatus()
         {
-            return "";
+            return DebugBuilder.ToString();
         }
 
         public string SerializeSubsystem()
@@ -55,15 +56,15 @@ namespace IngameScript
             foreach (var panel in Panels)
             {
                 panel.Value.ContentType = ContentType.SCRIPT;
+                panel.Value.ScriptBackgroundColor = new Color(1, 0, 0, 0);
             }
+
+            MapScale = MapOnScreenSizeMeters / MapSize;
         }
 
         public void Update(TimeSpan timestamp, UpdateFrequency updateFlags)
         {
-            DebugBuilder.Clear();
-            DebugBuilder.AppendLine(Turret.Azimuth.ToString());
-            DebugBuilder.AppendLine(Turret.Elevation.ToString());
-            DebugBuilder.AppendLine(Turret.IsShooting.ToString());
+            runs++;
 
             var a = Turret.Azimuth;
             var b = Turret.Elevation;
@@ -72,41 +73,257 @@ namespace IngameScript
             var WallPlane = new Plane(Panels[Vector2I.Zero].GetPosition() - Panels[Vector2I.Zero].WorldMatrix.Backward, Panels[Vector2I.Zero].WorldMatrix.Backward);
             Vector3 Intersection;
             TrigHelpers.PlaneIntersection(WallPlane, Turret.GetPosition() + Turret.WorldMatrix.Up * 0.579 + Turret.WorldMatrix.Backward * 0.068, TurretAimRay, out Intersection);
-            DebugBuilder.AppendLine((Intersection - Panels[Vector2I.Zero].GetPosition() + Panels[Vector2I.Zero].WorldMatrix.Backward * 1.25).Length().ToString());
 
-            var CursorDirection = Intersection - Panels[Vector2I.Zero].GetPosition() + Panels[Vector2I.Zero].WorldMatrix.Backward * 1.25;
+            var CursorDirection = Intersection - Panels[Vector2I.Zero].GetPosition() + Panels[Vector2I.Zero].WorldMatrix.Backward * 1.25 + Panels[Vector2I.Zero].WorldMatrix.Right * 1.25;
             var CursorDist = CursorDirection.Length();
             CursorDirection.Normalize();
 
-            var CursorPos = Vector3D.TransformNormal(CursorDirection, MatrixD.Transpose(Panels[Vector2I.Zero].WorldMatrix)) * CursorDist;
+            // LCD wall configuration:
+            // |  |    |
+            // |  |    |
+            // |  |    |
 
-            DebugBuilder.AppendLine(CursorPos.ToString());
-
-            var sprite = new MySprite(SpriteType.TEXTURE, "Cross", size: new Vector2(60f, 60f), color: Turret.IsShooting ? Color.Green : Color.Red);
-            sprite.Position = new Vector2((float)(CursorPos.X * 204.8 + 512), (float)(-CursorPos.Y * 204.8 + 256));
-            SpriteScratchpad.Clear();
-            SpriteScratchpad.Add(sprite);
-
-            foreach (var kvp in Panels)
+            // Cursor position, originating in the middle of the LCD wall, with unit in meters
+            var CursorPos = Vector3D.TransformNormal(CursorDirection, MatrixD.Transpose(Panels[Vector2I.Zero].WorldMatrix)) * CursorDist; 
+            var CursorPos2D = new Vector2((float)CursorPos.X, (float)CursorPos.Y);
+            
+            if (runs % 10 == 0)
             {
-                using (var frame = kvp.Value.DrawFrame())
+                DebugBuilder.Clear();
+                SinViewDegree = Math.Sin(ViewAngleDegrees * Math.PI / 180);
+                CosViewDegree = Math.Cos(ViewAngleDegrees * Math.PI / 180);
+
+                SpriteScratchpad.Clear();
+
+                AddSprite("CircleHollow", new Vector2(0, 0), new Vector2(5, 5 * (float)CosViewDegree));
+                for (int i = 10000; i <= MapSize; i += 10000)
                 {
-                    foreach (var spr in SpriteScratchpad)
-                    {
+                    var CircleSize = MapOnScreenSizeMeters * PixelsPerMeter * i / MapSize;
+                    AddSprite("CircleHollow", new Vector2(0, 0), new Vector2(CircleSize, CircleSize * (float)CosViewDegree), new Color(1f, 0.4f, 0f, 0.005f));
+                }
+
+                var gravDir = Controller.GetNaturalGravity();
+
+                // Create map matrix
+                if (gravDir != Vector3D.Zero)
+                {
+                    // Within gravity, align with gravity = down
+                    // Either use controller's forward direction or north as forward
+                    gravDir.Normalize();
+                    var flatNorthDir = new Vector3D(0, 1, 0) - VectorHelpers.VectorProjection(new Vector3D(0, 1, 0), gravDir);
+                    flatNorthDir.Normalize();
+                    var flatForwardDir = AlignWithNorth ? flatNorthDir : (Controller.WorldMatrix.Forward - VectorHelpers.VectorProjection(AlignWithNorth ? new Vector3D(0, 1, 0) : Controller.WorldMatrix.Forward, gravDir));
+                    flatForwardDir.Normalize();
+                    var flatLeftDir = Vector3D.Cross(flatForwardDir, gravDir);
+
+                    MapMatrix.Up = -gravDir;
+                    MapMatrix.Forward = flatForwardDir;
+                    MapMatrix.Left = flatLeftDir;
+
+                    var localNorthDir = Vector3D.TransformNormal(flatNorthDir, MatrixD.Transpose(MapMatrix));
+                    var localWestDir = Vector3D.Cross(localNorthDir, new Vector3D(0, -1, 0));
+
+                    AddTextSprite("N", LocalCoordsToMapPosition(localNorthDir * MapSize * 0.5, true), 4);
+                    AddTextSprite("S", LocalCoordsToMapPosition(-localNorthDir * MapSize * 0.5, true), 4);
+                    AddTextSprite("W", LocalCoordsToMapPosition(localWestDir * MapSize * 0.5, true), 4);
+                    AddTextSprite("E", LocalCoordsToMapPosition(-localWestDir * MapSize * 0.5, true), 4);
+                }
+                else
+                {
+                    // Else... idk just align with world axis?
+                    MapMatrix = MatrixD.Identity;
+                }
+
+                var intelItems = IntelProvider.GetFleetIntelligences(timestamp);
+                DebugBuilder.AppendLine(intelItems.Count.ToString());
+                foreach (var kvp in intelItems)
+                {
+                    Vector3D localCoords;
+                    Vector2 flatPosition, altitudePosition;
+                    GetMapCoordsFromIntel(kvp.Value, timestamp, out localCoords, out flatPosition, out altitudePosition);
+
+
+
+                    AddFleetIntelToMap(kvp.Value, timestamp, localCoords, flatPosition, altitudePosition);
+                }
+
+                // var cursor = new MySprite(SpriteType.TEXTURE, "White screen", size: new Vector2(60f, 60f), color: Turret.IsShooting ? Color.Green : Color.Red);
+                // cursor.Position = CursorPos2D;
+                // SpriteScratchpad.Add(cursor);
+
+                // AddSprite("White screen", new Vector2(1, 1), new Vector2(60, 60));
+                // AddSprite("Circle", new Vector2(1, 0), new Vector2(60, 60));
+                // AddSprite("CircleHollow", new Vector2(1, -1), new Vector2(60, 30));
+
+                var text = Turret.IsShooting ? "[><]" : "><";
+                AddTextSprite(text, CursorPos2D, 2, "Debug");
+
+                foreach (var kvp in Panels)
+                {
+                    DrawSpritesForPanel(kvp.Key);
+                }
+            }
+        }
+
+        Vector2 AddFleetIntelToMap(IFleetIntelligence intel, TimeSpan localTime, Vector3D localCoords, Vector2 flatPosition, Vector2 altitudePosition)
+        {
+            var color = Color.White;
+            if (intel.IntelItemType == IntelItemType.Friendly)
+            {
+                if ((((FriendlyShipIntel)intel).AgentStatus & AgentStatus.DockedAtHome) != 0)
+                    return Vector2.PositiveInfinity;
+                color = Color.Blue;
+            }
+            else if (intel.IntelItemType == IntelItemType.Enemy)
+            {
+                color = Color.Red;
+                var lastDetectedTime = localTime + IntelProvider.CanonicalTimeDiff - ((EnemyShipIntel)intel).LastValidatedCanonicalTime;
+                if (lastDetectedTime > TimeSpan.FromSeconds(4))
+                    color = new Color(1f, 0f, 0f, 0.002f);
+                else if (lastDetectedTime > TimeSpan.FromSeconds(3))
+                    color = new Color(1f, 0f, 0f, 0.005f);
+                else if (lastDetectedTime > TimeSpan.FromSeconds(2))
+                    color = new Color(1f, 0f, 0f, 0.007f);
+            }
+            else if (intel.IntelItemType == IntelItemType.Waypoint)
+            {
+
+            }
+            else
+            {
+                return Vector2.PositiveInfinity;
+            }
+
+            var middlePosition = (altitudePosition + flatPosition) * 0.5f;
+            var lineLength = MapScale * (float)localCoords.Y * (float)SinViewDegree;
+
+            if (intel.ID != Controller.CubeGrid.EntityId)
+            {
+                AddSprite("CircleHollow", flatPosition, new Vector2(30, 30 * (float)CosViewDegree), color);
+                AddSprite("CircleHollow", altitudePosition, new Vector2(20, 20), color);
+                AddSprite("SquareSimple", middlePosition, new Vector2(2, lineLength * PixelsPerMeter), color);
+            }
+
+            if (intel.IntelItemType == IntelItemType.Friendly)
+            {
+                AddSprite("CircleHollow", altitudePosition, new Vector2(2 * ScannerRange * MapScale * PixelsPerMeter), color);
+
+                if (Math.Abs((float)localCoords.Y) < ScannerRange)
+                {
+                    AddSprite("CircleHollow", flatPosition, new Vector2(2 * (float)Math.Sqrt(ScannerRange * ScannerRange - localCoords.Y * localCoords.Y) * MapScale * PixelsPerMeter, 2 * (float)(Math.Sqrt(ScannerRange * ScannerRange - localCoords.Y * localCoords.Y) * MapScale * PixelsPerMeter * CosViewDegree)), color);
+                }
+            }
+
+            return altitudePosition;
+        }
+
+        void GetMapCoordsFromIntel(IFleetIntelligence intel, TimeSpan localTime, out Vector3D localCoords, out Vector2 flatPosition, out Vector2 altitudePosition)
+        {
+            var worldCoords = intel.GetPositionFromCanonicalTime(localTime + IntelProvider.CanonicalTimeDiff);
+
+            localCoords = WorldCoordsToLocalCoords(worldCoords);
+            flatPosition = LocalCoordsToMapPosition(localCoords, true);
+            altitudePosition = LocalCoordsToMapPosition(localCoords);
+        }
+
+        Vector3D WorldCoordsToLocalCoords(Vector3D worldCoords)
+        {
+            var worldDir = worldCoords - Controller.GetPosition();
+            var worldDist = worldDir.Length();
+            worldDir.Normalize();
+            var localCoords = Vector3D.TransformNormal(worldDir, Matrix.Transpose(MapMatrix));
+            localCoords *= worldDist;
+            return localCoords;
+        }
+
+        private Vector2 LocalCoordsToMapPosition(Vector3D localCoords, bool flat = false)
+        {
+            return new Vector2(MapScale * (float)localCoords.X, -MapScale * (float)localCoords.Z * (float)CosViewDegree + (flat ? 0 : (MapScale * (float)localCoords.Y * (float)SinViewDegree)));
+        }
+
+        void AddSprite(string spriteType, Vector2 wallPosition, Vector2 size, Color? color = null)
+        {
+            var sprite = MySprite.CreateSprite(spriteType, wallPosition, size);
+            sprite.Color = color == null ? Color.White : color;
+            SpriteScratchpad.Add(sprite);
+        }
+
+        void AddTextSprite(string text, Vector2 position, int fontSize = 1, string font = "Debug")
+        {
+            TextMeasureBuilder.Clear();
+            TextMeasureBuilder.Append(text);
+            var sprite = MySprite.CreateText(text, font, Color.White, fontSize);
+            sprite.Size = Panels[Vector2I.Zero].MeasureStringInPixels(TextMeasureBuilder, sprite.FontId, sprite.RotationOrScale);
+            position.Y += sprite.Size.Value.Y * 0.5f / PixelsPerMeter;
+            sprite.Position = position;
+            SpriteScratchpad.Add(sprite);
+        }
+
+        Vector2 WallPositionToScreenPosition(Vector2I screenIndex, Vector2 wallPositionMeters)
+        {
+            var screenCenterMeters = ((Vector2)screenIndex) * 2.5f;
+            if (Panels[screenIndex].SurfaceSize.X == 1024)
+                screenCenterMeters.X += 1.25f;
+            var screenPosition = (wallPositionMeters - screenCenterMeters) * PixelsPerMeter;
+            screenPosition.Y *= -1; // Invert Y, because that's how the screen renders sprites
+            screenPosition += Panels[screenIndex].SurfaceSize * 0.5f;
+            return screenPosition;
+        }
+
+        void DrawSpritesForPanel(Vector2I screenIndex)
+        {
+            using (var frame = Panels[screenIndex].DrawFrame())
+            {
+                for (int i = 0; i < SpriteScratchpad.Count; i++)
+                {
+                    var spr = SpriteScratchpad[i];
+
+                    var originalPos = spr.Position.Value;
+
+                    spr.Position = WallPositionToScreenPosition(screenIndex, originalPos);
+
+                    if (spr.Position.Value.X + spr.Size.Value.X > 0 &&
+                        spr.Position.Value.X - spr.Size.Value.X < Panels[screenIndex].SurfaceSize.X &&
+                        spr.Position.Value.Y + spr.Size.Value.Y > 0 &&
+                        spr.Position.Value.Y - spr.Size.Value.Y < Panels[screenIndex].SurfaceSize.Y)
                         frame.Add(spr);
-                    }
+
+                    spr.Position = originalPos;
                 }
             }
         }
 
         MyGridProgram Program;
         StringBuilder DebugBuilder = new StringBuilder();
+        StringBuilder TextMeasureBuilder = new StringBuilder();
         IMyTerminalBlock ProgramReference;
 
+        int MapSize = 30000; // In meters
+        int PixelsPerMeter = 205; // 512 / 2.5
+        int ViewAngleDegrees = 60;
+        float MapOnScreenSizeMeters = 6.5f;
+        int ScannerRange = 1000;
+        float MapScale = 0;
+
+        int runs = 0;
+
+        // Required Subsystems
+        IIntelProvider IntelProvider;
+
+        public TacMapSubsystem(IIntelProvider intelProvider)
+        {
+            IntelProvider = intelProvider;
+        }
+
         List<MySprite> SpriteScratchpad = new List<MySprite>();
+        MatrixD MapMatrix = new MatrixD();
+        bool AlignWithNorth = false;
+        double SinViewDegree;
+        double CosViewDegree;
 
         // Components
         IMyLargeTurretBase Turret;
+        IMyShipController Controller;
         Dictionary<Vector2I, IMyTextPanel> Panels = new Dictionary<Vector2I, IMyTextPanel>();
 
         void GetParts()
@@ -118,6 +335,7 @@ namespace IngameScript
         {
             if (block.CubeGrid.EntityId != ProgramReference.CubeGrid.EntityId) return false;
             if (block is IMyLargeTurretBase) Turret = (IMyLargeTurretBase)block;
+            if (block is IMyShipController) Controller = (IMyShipController)block;
             if (block is IMyTextPanel)
             {
                 var startIndex = block.CustomName.IndexOf("[");
